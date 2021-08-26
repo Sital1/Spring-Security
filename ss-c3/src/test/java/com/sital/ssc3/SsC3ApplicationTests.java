39795bbefe69ad600f82b1b5e316519c7a0977d8
Sital Sitoula <heavywater221@gmail.com>
2021-08-26 12:33:38 -0500

    Implemented UserDetail Manager and used password encoder

---
+++ ss-c3/src/test/java/com/sital/ssc3/SsC3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sital.ssc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
