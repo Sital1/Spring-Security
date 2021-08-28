dff0ca8eb0e1e98d541dd61a60ffdca5441883f5
Sital Sitoula <sitalsitoula12@gmail.com>
2021-08-28 15:46:41 -0500

    initalized filterchain project

---
+++ filterchain/src/main/java/com/sital/filterchain/FilterchainApplication.java
@@ -0,0 +1,13 @@
+package com.sital.filterchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterchainApplication.class, args);
+    }
+
+}
---
+++ filterchain/src/test/java/com/sital/filterchain/FilterchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sital.filterchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
