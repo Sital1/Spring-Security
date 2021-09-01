cbc157561a0c9fd9c0fa8df710757fb01828066c
Sital Sitoula <sitalsitoula12@gmail.com>
2021-09-01 16:56:30 -0500

    intialized multiple authentication provider project

---
+++ multiple-authentication/src/main/java/com/sital/multipleauthentication/MultipleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.sital.multipleauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleAuthenticationApplication.class, args);
+	}
+
+}
---
+++ multiple-authentication/src/test/java/com/sital/multipleauthentication/MultipleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sital.multipleauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
