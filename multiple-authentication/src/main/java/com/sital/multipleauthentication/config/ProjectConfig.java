0691d696303f83f026f73170831ef132714cfdab
Sital Sitoula <heavywater221@gmail.com>
2021-09-01 22:43:17 -0500

    Configured Filters and Auth Providers

--- multiple-authentication/src/main/java/com/sital/multipleauthentication/config/ProjectConfig.java
+++ multiple-authentication/src/main/java/com/sital/multipleauthentication/config/ProjectConfig.java
@@ -1,14 +1,39 @@
 package com.sital.multipleauthentication.config;
 
+import com.sital.multipleauthentication.security.filters.UsernamePasswordAuthFilter;
+import com.sital.multipleauthentication.security.providers.OtpAuthenticationProvider;
+import com.sital.multipleauthentication.security.providers.UsernamePasswordAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+
+    private UsernamePasswordAuthenticationProvider authenticationProvider;
+
+    private OtpAuthenticationProvider otpAuthenticationProvider;
+
+    private UsernamePasswordAuthFilter filter;
+
+    @Autowired
+    public ProjectConfig(UsernamePasswordAuthenticationProvider authenticationProvider, OtpAuthenticationProvider otpAuthenticationProvider,
+                         UsernamePasswordAuthFilter filter) {
+        this.authenticationProvider = authenticationProvider;
+        this.otpAuthenticationProvider = otpAuthenticationProvider;
+        this.filter = filter;
+    }
+
+
 
     // override the methods
     // userDetail service
@@ -26,5 +51,18 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+      auth.authenticationProvider(otpAuthenticationProvider)
+              .authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAt(filter,
+                BasicAuthenticationFilter.class
+        );
     }
 }
--- multiple-authentication/src/main/java/com/sital/multipleauthentication/security/providers/OtpAuthenticationProvider.java
+++ multiple-authentication/src/main/java/com/sital/multipleauthentication/security/providers/OtpAuthenticationProvider.java
@@ -8,10 +8,12 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class OtpAuthenticationProvider implements AuthenticationProvider {
 
     private OtpRepository otpRepository;
