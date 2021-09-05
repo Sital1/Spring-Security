f9f15f0511f07c9fbf93737e9ca71049cff7adda
Sital Sitoula <heavywater221@gmail.com>
2021-09-04 23:52:58 -0500

    csrf disabled

---
+++ csrf/src/main/java/com/sital/csrf/config/ProjectConfig.java
@@ -0,0 +1,18 @@
+package com.sital.csrf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        // generally don't disable the csrf
+        http.csrf().disable();
+
+    }
+}
