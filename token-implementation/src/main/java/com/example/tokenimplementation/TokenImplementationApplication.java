7da824cf7d63bca2d10e943fb0f8a716b86c2cb6
Sital Sitoula <heavywater221@gmail.com>
2021-09-18 13:22:55 -0500

    initialized token implementation

---
+++ token-implementation/src/main/java/com/example/tokenimplementation/TokenImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokenimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenImplementationApplication.class, args);
+    }
+
+}
---
+++ token-implementation/src/test/java/com/example/tokenimplementation/TokenImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tokenimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
