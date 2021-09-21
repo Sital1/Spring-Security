fb58e11c84e49f5992800ce4b10344d1a61fbe8a
Sital Sitoula <heavywater221@gmail.com>
2021-09-21 14:54:07 -0500

    initialized auth server to use blackboarding to verify tokens

---
+++ auth-server-opaquetokens-blackboarding/src/main/java/com/sital/authserveropaquetokensblackboarding/AuthServerOpaquetokensBlackboardingApplication.java
@@ -0,0 +1,13 @@
+package com.sital.authserveropaquetokensblackboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerOpaquetokensBlackboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerOpaquetokensBlackboardingApplication.class, args);
+    }
+
+}
---
+++ auth-server-opaquetokens-blackboarding/src/test/java/com/sital/authserveropaquetokensblackboarding/AuthServerOpaquetokensBlackboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sital.authserveropaquetokensblackboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerOpaquetokensBlackboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
