1e8a879d5b36309a69d99f8980c3c0e2609ad63b
Sital Sitoula <heavywater221@gmail.com>
2021-09-21 00:17:07 -0500

    UserDetailsService DI for refres_token fix

--- token-implementation/src/main/java/com/example/tokenimplementation/TokenImplementationApplication.java
+++ token-implementation/src/main/java/com/example/tokenimplementation/TokenImplementationApplication.java
@@ -11,6 +11,14 @@ public class TokenImplementationApplication {
 
         /*
         Opaque tokens are mainly used for verifying and don't have any data in them.
+
+        Verify Token Methods:
+             1. Resource Server Calls the authorization server via /check endpoint.
+             2. Blackboarding database. A shared database between Resource Server and authorization Server. Usually avoid having shared database.
+
+             Soln only in examples
+               Resource Server and Authorization Server are the Same application.
+
          */
 
     }
--- token-implementation/src/main/java/com/example/tokenimplementation/config/AuthServerConfig.java
+++ token-implementation/src/main/java/com/example/tokenimplementation/config/AuthServerConfig.java
@@ -3,6 +3,7 @@ package com.example.tokenimplementation.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -10,21 +11,29 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
+/*
+
+ */
+
+
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
 
     @Autowired
-    public AuthServerConfig(AuthenticationManager authenticationManager) {
+    public AuthServerConfig(AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
         this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
     }
-
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.passwordEncoder(NoOpPasswordEncoder.getInstance());
+        security.passwordEncoder(NoOpPasswordEncoder.getInstance())
+                //By default check token is un-authorized. Should specify
+                .checkTokenAccess("isAuthenticated()"); // isAuthenticated
     }
 
     @Override
@@ -33,11 +42,14 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .withClient("client1")
                 .secret("secret1")
                 .authorizedGrantTypes("password","refresh_token")
+                .accessTokenValiditySeconds(5000)
                 .scopes("read");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
         endpoints.authenticationManager(authenticationManager);
+        endpoints.userDetailsService(userDetailsService);
     }
 }
