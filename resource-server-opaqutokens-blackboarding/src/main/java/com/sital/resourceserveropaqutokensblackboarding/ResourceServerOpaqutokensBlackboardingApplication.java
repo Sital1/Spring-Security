cad476da02a1a86c27afaa31a665b2d09abc0af0
Sital Sitoula <heavywater221@gmail.com>
2021-09-21 16:45:33 -0500

    initialized resource server for opaque token  blackboarding

---
+++ resource-server-opaqutokens-blackboarding/src/main/java/com/sital/resourceserveropaqutokensblackboarding/ResourceServerOpaqutokensBlackboardingApplication.java
@@ -0,0 +1,13 @@
+package com.sital.resourceserveropaqutokensblackboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerOpaqutokensBlackboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerOpaqutokensBlackboardingApplication.class, args);
+    }
+
+}
