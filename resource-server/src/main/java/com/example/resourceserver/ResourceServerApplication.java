e2383c8cfe462fec1cd3c646bd2fbab734507347
Sital Sitoula <heavywater221@gmail.com>
2021-09-21 14:26:29 -0500

    initialized resource server

---
+++ resource-server/src/main/java/com/example/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+
+        /*
+        Resource server is the back-end we secure
+         */
+    }
+
+}
---
+++ resource-server/src/main/java/com/example/resourceserver/config/ResourceServerConfig.java
@@ -0,0 +1,9 @@
+package com.example.resourceserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig {
+}
