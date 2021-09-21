a2c47d642a48f3cd030c3c3545760748f2aed2f6
Sital Sitoula <heavywater221@gmail.com>
2021-09-21 14:53:12 -0500

    Used introspection to validate token

--- resource-server/src/main/java/com/example/resourceserver/config/ResourceServerConfig.java
+++ resource-server/src/main/java/com/example/resourceserver/config/ResourceServerConfig.java
@@ -1,9 +1,22 @@
 package com.example.resourceserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        super.configure(resources);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
